0ee4a8e3c85bdb2e7d30ef1ed0d9cb07cf8a93c0
Anastasia Slezko <slezkoanastasiia@gmail.com>
2022-05-17 10:14:19 +0300

    3 sample
    3 sample

--- js/task-08.js
+++ js/task-08.js
@@ -4,42 +4,22 @@ const passwordEl = document.querySelector(".password");
 
 function onSubmitForm(event) {
   event.preventDefault();
-  const formElements = event.currentTarget.elements;
-  console.log(formElements);
-  const formData = new FormData(event.currentTarget);
-  formData.forEach((value, name) => {
-    console.log("onSubmitForm -> name", name);
-    console.log("onSubmitForm -> value", value);
-  });
-  // const email = formElements.email.value;
-  // const password = formElements.password.value;
-  // console.log(email, password);
-  // const formData = {
-  //   email,
-  //   password,
-  // };
-  // console.log(formData);
+
   const {
     elements: { email, password },
   } = event.currentTarget;
 
   if (email.value === "" || password.value === "") {
     alert(" Все поля должны быть заполнены!");
+    return;
   }
   if (password.value.length <= 8) {
     alert("Длинна пароля должна быть более 8 символов");
+    return;
   }
-  // console.log(`Email: ${email.value}, Password: ${password.value}`);
+
   console.log({ [email.name]: email.value, [password.name]: password.value });
   event.currentTarget.reset();
 }
-
-// function onInputForm() {
-//   if (!email.value) {
-//     email.textContent = "Все поля должны быть заполнены!";
-//     return;
-//   }
-// }
-
 form.addEventListener("submit", onSubmitForm);
 // email.addEventListener("input", onInputForm);
